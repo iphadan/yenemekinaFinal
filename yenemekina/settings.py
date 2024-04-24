38bad56857dda98e728199e0b243c58296cac8e0
iphadan <ephadaniel177@gmail.com>
2024-04-24 17:00:00 +0300

    upload image to be solved

--- yenemekina/settings.py
+++ yenemekina/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=['static/']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'images/'
+MEDIA_URL = '/images/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'images')
 
 # Default primary key field type
